0a69bb17490f0e2e7f86272b62e5697d2a035bf3
minhaz <mebjas@gmail.com>
2013-09-01 12:13:55 +0530

    db library
    presently supports db connection and disconnecions

---
+++ libs/db.php
@@ -0,0 +1,31 @@
+<?php
+/*
+this library will handle all database operations 
+including features to connect and disconnect
+we shall be using mysql_ functions instead of mysqli_ functions for now
+shall change later 
+*/
+/*
+class dbase: handles database connection and disconnecion operation
+uses static functions and mysql_ fucntions
+expects config page to be included
+*/
+require_once '../configs/config.ini.php';	//contains db configuration file
+class dbase{
+	public static $con;
+	public static function start_connection()
+	{
+		self::$con = mysql_connect(HOST,USER,PASSWORD);
+		mysql_select_db(_DB_MAIN,self::$con);
+	}
+	public static function close_connection()
+	{
+		if(isset(self::$con))
+		{
+			mysql_close(self::$con);
+		}
+	}
+}
+
+
+?>
