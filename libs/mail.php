2f36704d69729d81aba755bd2063bb9a3de1ead0
minhaz <mebjas@gmail.com>
2013-09-10 09:31:19 +0530

    mail library to hold mailing features classes and functions
    two classes
    mail and bulkmail that inherit mails........

---
+++ libs/mail.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * this class will handle all mail operations like sending individual || bulk mails ||
+ * framing of the mail ... the format [UI] of mail shall be kept in a XML default one
+ */
+ 
+ /*
+  * class to send a single mail, frame a mail , save mail UI to file
+ */
+class mail
+{
+	
+};
+ 
+ /*
+	class to send bulk mails from array of email ids or from database directly
+ */
+class bulkmail extends mail
+{
+
+};
+
+?>
