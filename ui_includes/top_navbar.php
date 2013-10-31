742055ed9cda9e736a742b89c1827b0e426fbd46
minhaz <mebjas@gmail.com>
2013-10-31 17:57:05 +0530

    logout dropdown replaced by normal button
    we do not need a UI dropdown for logout here

--- ui_includes/top_navbar.php
+++ ui_includes/top_navbar.php
@@ -37,16 +37,11 @@
 		</div>
 	<!-- theme selector ends -->	
 	<!-- user dropdown starts -->
-		<div class="btn-group pull-right" >
-			<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-				<i class="icon-user"></i><span class="hidden-phone"> admin</span>
-				<span class="caret"></span>
+		<div class="btn-group pull-right">
+			<a class="btn" href="logout.php">
+				<i class="icon-user"></i>
+				<span class="hidden-phone"> logout </span>
 			</a>
-			<ul class="dropdown-menu">
-				<li><a href="#">Profile</a></li>
-				<li class="divider"></li>
-				<li><a href="login.html">Logout</a></li>
-			</ul>
 		</div>
 	<!-- user dropdown ends -->
 		
