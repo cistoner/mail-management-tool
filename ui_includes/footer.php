f0b7038a8f95aec6c627f491dc619442ad4327c5
minhaz <mebjas@gmail.com>
2013-10-31 17:49:39 +0530

    format page for MMT
    @folder ui_includes contain UI files that are required in each UI page
    of MMT
    @index.php host the code for dashboard of MMT

---
+++ ui_includes/footer.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * This page contain the UI section of footer
+ * Can be added when needed will be applied to every page
+ * class in the <i> tag gives the icon to the menu item
+ * <a> gives the onclick link
+ * (c) Cistoner Inc.
+ */
+/**
+ * change the Cistoner link to product link or about us link
+ */
+?>
+<footer>
+	<p class="pull-left">
+		Product by <a href="http://cistoner.com"> Cistoner Inc </a>&copy; 2012-13 And Theme by 
+	 	<a href="http://usman.it" target="_blank">Muhammad Usman</a> 2012
+	</p>
+</footer>
---
+++ ui_includes/sidebar.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * This page contain the UI section of sidebar elements
+ * Can be added when needed will be applied to every page
+ * class in the <i> tag gives the icon to the menu item
+ * <a> gives the onclick link
+ * (c) Cistoner Inc.
+ */
+?>
+<div class="span2 main-menu-span">
+	<div class="well nav-collapse sidebar-nav">
+		<ul class="nav nav-tabs nav-stacked main-menu">
+			<li class="nav-header hidden-tablet">Main</li>
+			<li><a class="ajax-link" href="index.php"><i class="icon-home"></i><span class="hidden-tablet"> Dashboard </span></a></li>
+			<li><a class="ajax-link" href="#"><i class="icon-eye-open"></i><span class="hidden-tablet"> Sample </span></a></li>
+		</ul>
+	</div><!--/.well -->
+</div><!--/span-->
