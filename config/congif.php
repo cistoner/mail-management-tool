e45e4530a4b5c9430aff5608c94d432cd260a901
minhaz <mebjas@gmail.com>
2013-11-01 21:49:34 +0530

    added dummy config file for testing

---
+++ config/congif.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * dummy config file 
+ * this is the format of config file that shall be used by
+ * all data here shall be taken by user at the time of installation
+ */
+ define("username_key","");
+ define("password_key","");
