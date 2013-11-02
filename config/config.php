eea837feb779c711d0cfefe880dc5ea1168afee1
minhaz <mebjas@gmail.com>
2013-11-02 15:45:06 +0530

    index.php added to locations to redirect users
    users are redirected to root directory of the server in case
    of invalid access to these directories

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * dummy config file 
+ * this is the format of config file that shall be used by
+ * all data here shall be taken by user at the time of installation
+ */
+ define("username_key","");
+ define("password_key","");
+ define("password_salt","temporarypasswordsalt");
