1f62eae07fd38d2a75699504fa1ee6b384323589
minhaz <mebjas@gmail.com>
2013-11-12 06:27:30 +0530

    ui fixes

--- ui_includes/footer.php
+++ ui_includes/footer.php
@@ -10,6 +10,9 @@
  * change the Cistoner link to product link or about us link
  */
 ?>
+<!-- jQuery -->
+<script src="js/jquery-1.7.2.min.js"></script>
+<script src="js/mmt.js"></script>
 <footer>
 	<p class="pull-left">
 		Powered by <a href="http://cistoner.com"> Cistoner Inc </a>&copy; 2012-13 And <cite>Theme by 
